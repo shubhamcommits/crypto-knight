93a06c36e087344cfb8449326010164d07489959
AdiSood97 <advityasood@gmail.com>
2021-05-14 12:27:36 +0530

    portfolio..if doesnt exist...then working

--- api/models/index.js
+++ api/models/index.js
@@ -2,5 +2,6 @@ const Auth = require('./auth.model')
 const User = require('./user.model')
 const Trigger = require('./trigger.model')
 const Favcoin = require('./favcoin.model')
+const Portfolio = require('./portfolio.model')
 
-module.exports = { Auth, User, Trigger, Favcoin }
+module.exports = { Auth, User, Trigger, Favcoin, Portfolio }
