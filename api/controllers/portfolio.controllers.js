b57195bb4d68b0e31071c9a3ad4e46b486f0e3e2
AdiSood97 <advityasood@gmail.com>
2021-05-14 16:34:45 +0530

    portfolio get api

--- api/controllers/portfolio.controllers.js
+++ api/controllers/portfolio.controllers.js
@@ -29,7 +29,36 @@ const PortfolioControllers = {
                 error: error
             })
         }
-    }
+    },
+    async getPortfolio(req, res, next) {
+        try {
+
+            // Fetch the data from the params
+            let { userId } = req.params
+
+            // call the get user function
+            PortfolioService.getPortfolio(userId)
+                .then((data) => {
+
+                    // Send Status 200 response
+                    return res.status(200).json({
+                        message: 'User portfolio has been fetched successfully!',
+                        user: data
+                    })
+                })
+                .catch((error) => {
+                    return res.status(500).json({
+                        message: 'Internal server error!',
+                        error: error
+                    })
+                })
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Internal server error!',
+                error: error
+            })
+        }
+    },
 }
 
 module.exports = PortfolioControllers
